b0c7457b9438f9cb8e4812e5f0842eabe60e0911
TyranZilla <tryaniks@gmail.com>
2022-08-25 04:25:50 +0200

    first commit

---
+++ turtle/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tryaniks/turtle/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tryaniks/turtle/devel;/opt/ros/noetic'.split(';') if '/home/tryaniks/turtle/devel;/opt/ros/noetic' != '' else []
---
+++ turtle/build/package1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tryaniks/turtle/src/package1
+# Build directory: /home/tryaniks/turtle/build/package1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
